9f533b4c9764c43976a9aabfe43e7495797535ca
Luna Wang <zlunawang@gmail.com>
2026-03-15 00:08:38 -0700

    add timed pivot up/down commands for intake

---
+++ src/main/java/frc/robot/commands/intake/pivot/PivotDownTimedCommand.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.intake.pivot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
+
+public class PivotDownTimedCommand extends Command {
+    private final PivotIntakeSubsystem pivotIntake;
+    private final Timer timer = new Timer();
+
+    public PivotDownTimedCommand(PivotIntakeSubsystem pivotIntake) {
+        this.pivotIntake = pivotIntake;
+        addRequirements(pivotIntake);
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        pivotIntake.setManualSpeed(IntakeConstants.PIVOT_DOWN_DUTY_CYCLE);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(IntakeConstants.PIVOT_DOWN_DURATION_SECONDS);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        pivotIntake.stop();
+        timer.stop();
+    }
+}
---
+++ src/main/java/frc/robot/commands/intake/pivot/PivotUpTimedCommand.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.intake.pivot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
+
+public class PivotUpTimedCommand extends Command {
+    private final PivotIntakeSubsystem pivotIntake;
+    private final Timer timer = new Timer();
+
+    public PivotUpTimedCommand(PivotIntakeSubsystem pivotIntake) {
+        this.pivotIntake = pivotIntake;
+        addRequirements(pivotIntake);
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        pivotIntake.setManualSpeed(IntakeConstants.PIVOT_UP_DUTY_CYCLE);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(IntakeConstants.PIVOT_UP_DURATION_SECONDS);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        pivotIntake.stop();
+        timer.stop();
+    }
+}
