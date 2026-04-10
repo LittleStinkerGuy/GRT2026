36446af1dd29ac0a4a772ad06e671a5c30a08c4a
Luna Wang <zlunawang@gmail.com>
2026-04-10 13:45:00 -0700

    centwr auotn

---
+++ src/main/java/frc/robot/commands/auton/CenterAuton.java
@@ -0,0 +1,9 @@
+package frc.robot.commands.auton;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+public class CenterAuton extends PathPlannerAuto {
+    public CenterAuton() {
+        super("center");
+    }
+}
