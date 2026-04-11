4299ff66ba65a6cb23504e2564aa560ea48bc702
Luna Wang <zlunawang@gmail.com>
2026-04-11 16:05:57 -0700

    isolate itnake path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -418,7 +418,7 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return new TIMECNeutralIntakeTOWERAuton(flywheelSubsystem, hoodSubsystem, tower, HopperSubsystem, pivotIntake, intakeSubsystem, learner);
+        return new PathPlannerAuto("isolatedINTAKE");
         // return new PathPlannerAuto("auton2");
         // return new NeutralDefenseAuton(flywheelSubsystem, hoodSubsystem, tower, HopperSubsystem, pivotIntake, intakeSubsystem);
         // return new ANeutralIntakeAuton(flywheelSubsystem, hoodSubsystem, tower, HopperSubsystem, pivotIntake, intakeSubsystem);
