5ee0aacd857188119d3dc1f29bfc1b8e8c507c47
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-05-02 16:18:19 -0700

    move pivot files to folder

--- src/main/java/frc/robot/subsystems/intake/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIO.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.intake;
+package frc.robot.subsystems.intake.pivot;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Celsius;
--- src/main/java/frc/robot/subsystems/intake/pivot/PivotIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIOTalonFX.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.intake;
+package frc.robot.subsystems.intake.pivot;
 
 import static edu.wpi.first.units.Units.Rotations;
 import static frc.robot.util.PhoenixUtil.tryUntilOk;
