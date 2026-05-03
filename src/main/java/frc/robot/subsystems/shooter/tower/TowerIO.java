e5bffd37343aa38fb5abdcc082615cb3ffa9d47f
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-05-03 01:44:28 -0700

    feat: implement TowerIO interface and TowerSubsystem for tower control and monitoring

---
+++ src/main/java/frc/robot/subsystems/shooter/tower/TowerIO.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems.shooter.tower;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+import org.littletonrobotics.junction.AutoLog;
+import edu.wpi.first.units.AngularAccelerationUnit;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.util.ComponentStatus.MotorControlMode;
+import frc.robot.util.PIDConstants;
+
+public interface TowerIO {
+    @AutoLog
+    public static class TowerIOInputs {
+        public Angle position = Rotations.of(0);
+        public AngularVelocity velocity = RotationsPerSecond.of(0);
+        public AngularAcceleration acceleration = RotationsPerSecondPerSecond.of(0);
+        public Voltage appliedVoltage = Volts.of(0);
+        public Current supplyCurrent = Amps.of(0);
+        public Current torqueCurrent = Amps.of(0);
+        public Current statorCurrent = Amps.of(0);
+        public Temperature temp = Celsius.of(0);
+        public boolean tempFault = false;
+        public boolean connected = false;
+
+        public MotorControlMode controlMode = MotorControlMode.Disabled;
+        public double appliedDutyCycle = 0.0;
+        public double closedLoopSetpoint = 0.0;
+        public double closedLoopOutput = 0.0;
+    }
+
+    default void updateInputs(TowerIOInputs inputs) {}
+
+    default PIDConstants getDefaultPID() {
+        return PIDConstants.ZERO;
+    }
+
+    default void updatePID(double kP, double kI, double kD, double kS, double kV, double kA) {}
+
+    default void updateMotionMagicConfig(AngularAcceleration accel, AngularVelocity velo, Velocity<AngularAccelerationUnit> jerk) {}
+
+    default void setDutyCycleOut(double dutyCycle) {}
+
+    default void setVoltageOut(Voltage voltsOut) {}
+
+    default void setVelocityOut(AngularVelocity velocityOut) {}
+
+    default void stop() {}
+
+}
