df1e810b6f9c52e5fc7a23e29999c12c66d08015
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-05-04 19:58:31 -0700

    fix: update ModuleLimits record formatting for consistency

--- src/main/java/frc/robot/util/ModuleLimits.java
+++ src/main/java/frc/robot/util/ModuleLimits.java
@@ -1,5 +1,4 @@
 package frc.robot.util;
 
 public record ModuleLimits(
-    double maxDriveVelocity, double maxDriveAcceleration, double maxSteeringVelocity) {
-}
+    double maxDriveVelocity, double maxDriveAcceleration, double maxSteeringVelocity) {}
