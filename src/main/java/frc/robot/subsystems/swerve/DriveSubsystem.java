220b9a43294456f3e8e415621fc2793948896080
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-05-08 15:17:57 -0700

    make checkstyle stop complaining

--- src/main/java/frc/robot/subsystems/swerve/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/DriveSubsystem.java
@@ -24,7 +24,7 @@ public class DriveSubsystem {
         public String toKey() {
             return fullName.replace(" ", "");
         }
-    };
+    }
 
     static final Lock ODOMETRY_LOCK = new ReentrantLock();
 
